f75c04ec04c4e9ea4eb6b32b99a780a674aaa3eb
Matt <matt@mcburnie.ca>
2018-05-12 22:44:36 -0400

    Testing again

--- Unit7/src/unit7puttingitalltogether/QuotationsDatabase.java
+++ Unit7/src/unit7puttingitalltogether/QuotationsDatabase.java
@@ -3,7 +3,7 @@ package unit7puttingitalltogether;
 public class QuotationsDatabase {
 
 	public static void main(String[] args) {
-		System.out.println("I shared a thing");
+		System.out.println("I shared a thing, again!");
 
 	}
 
