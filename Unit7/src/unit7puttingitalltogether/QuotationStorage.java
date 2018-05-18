0918fbe6cb8791cb2c12e89d13cd6ae9895c34db
Matt <matt@mcburnie.ca>
2018-05-18 18:31:45 -0400

    Added a File Directory
    The program should be able to access the text file from any computer.

--- Unit7/src/unit7puttingitalltogether/QuotationStorage.java
+++ Unit7/src/unit7puttingitalltogether/QuotationStorage.java
@@ -4,66 +4,129 @@ import java.util.*;
 import javax.swing.*;
 
 public class QuotationStorage {
-        private int indexNum;
-        private String quoteStr;
-	ArrayList<String> storage = new ArrayList();
-        ArrayList<String> authorList = new ArrayList();
-	public QuotationStorage(ArrayList<String> storage_, ArrayList<String> authorList_) {
-		storage = storage_;
-                authorList = authorList_;
-	}
-	
-	public String toString() {
-		String output = "";
-		for(int i = 0; i < storage.size(); i++) {
-			output += "[" + i + "] " + storage.get(i) + "\n";
-                        
-		}
-		return output;
-	}
-        public void removeQuote(String quoteStr)
-        {
-            indexNum = storage.indexOf(quoteStr);
-                storage.remove(indexNum);
-                authorList.remove(indexNum);
-            
+    private int indexNum;
+    private String quoteStr;
+    //Arraylist to store index of authors
+    ArrayList<String> authIndex = new ArrayList();
+    //arraylist for all quotes
+    ArrayList<String> storage = new ArrayList();
+    //parallel arraylist to store authors for each quote individually
+    ArrayList<String> authorList = new ArrayList();
+    
+    //default constructor to fill arrayList's
+    public QuotationStorage(ArrayList<String> storage_, ArrayList<String> authorList_) {
+        storage = storage_;
+        authorList = authorList_;
+    }
+    //toString to print all quotes out
+    public String toString() {
+        String output = "";
+        for(int i = 0; i < storage.size(); i++) {
+            output += "[" + i + "] " + storage.get(i) + "\n";
         }
-        public void addQuote(String auth, String quote)
-        {
-            quoteStr = "\""+quote+"\" - "+auth;
-            storage.add(quoteStr);
-            authorList.add(auth);
+        return output;
+    }
+    //method to remove quote
+    public void removeQuote(String quoteStr)
+    {
+        //user inputs a selected quote. The index value of said quote is obtained
+        indexNum = storage.indexOf(quoteStr);
+        //.remove is used with the index position to remove both the author and the quote
+        storage.remove(indexNum);
+        authorList.remove(indexNum);
+
+    }
+    //method to add quote
+    public void addQuote(String auth, String quote)
+    {
+        //gets the user inputted quote and name
+        //adds "" marks around the quote followed by - author and adds it to the
+        //quotes arraylist
+        quoteStr = "\""+quote+"\" - "+auth;
+        storage.add(quoteStr);
+        //authors name is added to the author list array
+        authorList.add(auth.toUpperCase());
+        sort();
+    }
+    //generates list of all authors used for search by author
+    public void authorIndex()
+    {
+       authIndex.clear();
+       //counter to look through all authors
+       //ensures author name doesnt appear more than once
+       for (int c = 0; c < authorList.size(); c++)
+       {
+           if (c == 0)
+           {
+               //when the rogram first starts, the first object is added because
+               //there is no prior object to compare to to check for duplicates
+               authIndex.add(authorList.get(c));
+           }
+            //since array is sorted, if the author's name has already been printed,
+            //it which is shown by c-1, it will skip it so that there will be no
+            //duplciate author names
+           else if (!authorList.get(c).equalsIgnoreCase(authorList.get(c-1)))
+           {
+               authIndex.add(authorList.get(c));
+           }
+       }
+    }
+    //searches for quotes by author selected in prior method
+    public void search(String V)
+    {
+        //sorted linear search
+        int k;
+        //intitailizes temporary arraylist to store the index positions of quotes
+        //by a specific author
+        ArrayList<Integer> quoteIndex = new ArrayList();
+        for(k = 0; k < authorList.size(); k++){
+            // if a quote is said by the author they are looking for, it gets
+            //the index position represented by k
+            if(authorList.get(k).equalsIgnoreCase(V)) {
+                quoteIndex.add(k);
+            }
+            //if the author's name value is passed, it will break the loop
+            if(authorList.get(k).compareTo(V)> 0){
+                break;
+            }
         }
-        public void search(String V)
+        //prints out all quotes by specified author.
+        System.out.println("All the quotes by "+V+" are:");
+        System.out.println("============================================");
+        for (int n = 0; n < quoteIndex.size(); n++)
         {
-            int k;
-            ArrayList<Integer> quoteIndex = new ArrayList();
-            int counter = 0;
-            for(k = 0; k < authorList.size(); k++){
-                if(authorList.get(k).equalsIgnoreCase(V)) {
-                    counter++;
-                    quoteIndex.add(k);
-                }
-                if(authorList.get(k).compareTo(V)> 0){
-                    break;
+            System.out.println(storage.get(quoteIndex.get(n)));
+        }
+        System.out.println("============================================");
+        }
+        //bubble sort
+        public void sort() {
+            //sorts the author array by comparing its values
+            //goes to swap after determining that element > element+1
+            for (int pass = 1; pass < authorList.size(); pass++) {
+                for (int element=0;element<authorList.size()-1;element++) {
+                    if (authorList.get(element).compareTo(authorList.get(element + 1)) > 0) {
+                        swap(element, element + 1); 
+                    }
                 }
             }
-            if (counter > 0)
-            {
-                System.out.println("All the quotes by "+V+" are:");
-                System.out.println("============================================");
-                for (int n = 0; n < quoteIndex.size(); n++)
-                {
-                    System.out.println(storage.get(quoteIndex.get(n)));
-                }
-                System.out.println("============================================");
-            }
-            else
-            {
-                JOptionPane.showMessageDialog(null,"The author: "+V+" could not be found!");
-            }
-        }
-        
-        
-        
+      }
+        //swap method for sort
+      public void swap(int first, int second) {
+          //two swaps are performed, one to swap author list and the other to
+          //swap the quotes because they are stored seperately
+          //first strings hold the first author's name and the first quote
+            String holdAuth = authorList.get(first);
+            String holdQuot = storage.get(first);
+            //sets the quote and author name in the first position to the 
+            //quote and author in the second postition
+            authorList.set(first, authorList.get(second));
+            storage.set(first, storage.get(second));
+            //sets the quote and author in the second  position to the quote
+            //and author in the first position
+            authorList.set(second, holdAuth);
+            storage.set(second, holdQuot);
+      } 
+
+
 }
--- Unit7/src/unit7puttingitalltogether/QuotationsDatabase.java
+++ Unit7/src/unit7puttingitalltogether/QuotationsDatabase.java
@@ -1,3 +1,4 @@
+
 package unit7puttingitalltogether;
 
 import java.io.*;
@@ -6,130 +7,198 @@ import javax.swing.*;
 
 public class QuotationsDatabase {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		String user = "";
-		String authName;
-		String quoteInput;
-		String removeInput;
-		int userDialog = 0;
-		String fileName = "quotes.txt";
-		
-		String dir = System.getProperty("user.dir");
-		System.out.println(dir + "\\src\\unit7puttingitalltogether\\quotes.txt");
-		
-		String[] options = { "Display all quotes", "Display random quote", "Display selected quote", "Search by Author",
-				"Add a quote", "Remove a quote", "Exit" };
-		BufferedReader quoteReader1 = null;
-		try {
-			quoteReader1 = new BufferedReader(new FileReader(dir + "\\src\\unit7puttingitalltogether\\quotes.txt"));
-		} 
-		catch (FileNotFoundException e) {
-			JOptionPane.showMessageDialog(null, "File: " + fileName + " not found.");
-			System.exit(0);
-		}
-		ArrayList<String> authors = new ArrayList();
-		ArrayList<String> storage = new ArrayList();
-		while (true) {
-			String line1 = quoteReader1.readLine();
-			if (line1 == null) {
-				break;
-			} 
-			else {
-				String charAt = Character.toString(line1.charAt(0));
-				if (charAt.equalsIgnoreCase("-")) {
-					authors.add(line1.substring(2, line1.length()));
-				} 
-				else {
-					storage.add(line1);
-				}
-			}
-		}
-		quoteReader1.close();
-		QuotationStorage quotes = new QuotationStorage(storage, authors);
+        boolean exit;
+        String user = "";
+        String authName = "";
+        String quoteInput = "";
+        String removeInput;
+        ArrayList<String> authors = new ArrayList();
+        ArrayList<String> storage = new ArrayList();
+        int userDialog = 0;
+        
+        
+        String location = System.getProperty("user.dir") + "\\src\\unit7puttingitalltogether\\quotes2.txt";
+        String[] options = {"Display all quotes", "Display random quote", "Display selected quote", "Search by Author","Add a quote", "Remove a quote", "Exit"};
+        BufferedReader quoteReader1 = null;
+        PrintWriter fileWriter1 = null;
+        //error check to make sure file in given directory exists
+        try{
+            quoteReader1 = new BufferedReader(new FileReader(location));
+        }
+        catch(FileNotFoundException e)
+        {
+            JOptionPane.showMessageDialog(null, "File not found");
+            quoteReader1.close();
+            System.exit(0);
+        }	
+        //Fills the storage and authors array list with respective info
+        while(true)
+        {
+            //line reader
+            String line1 = quoteReader1.readLine();
+            //if line is null (end of file), exits loop
+            if (line1 == null)
+            {
+                break;
+            }
+            else{
+                //temp variable to dettermine first letter in a line
+                String charAt = Character.toString(line1.charAt(0));
+                //if the first letter is - (representing an author, adds said 
+                //line to authors arraylist
+                if (charAt.equalsIgnoreCase("-")){
+                    authors.add(line1.substring(2, line1.length()));
+                }
+                //else, the line must be a quote so it adds it to storage arraylist
+                else{
+                    storage.add(line1);
+                }
+            }
+        }
+        //closes reader
+        quoteReader1.close();
+        //intitalized constructor to modify the quotations array (add, remove, etc)
+        QuotationStorage quotes = new QuotationStorage(storage, authors);
+        //if user clicks exit, user dialog will output 6, therefore the program exits
+        while(userDialog != 6) {
+            //main menu
+            userDialog = JOptionPane.showOptionDialog(null, "Please choose an option", "Quotations Database", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
 
-		while (userDialog != 6) {
+            // DISPLAY ALL QUOTES
+            if(userDialog == 0) {
 
-			userDialog = JOptionPane.showOptionDialog(null, "Please choose an option", "Quotations Database",
-					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+                    System.out.println("============================================");
+                    System.out.println(quotes);
+                    System.out.println("============================================");
 
-			// DISPLAY ALL QUOTES
-			if (userDialog == 0) {
+            }
 
-				System.out.println("============================================");
-				System.out.println(quotes);
-				System.out.println("============================================");
+            // DISPLAY RANDOM QUOTE
+            else if(userDialog == 1) {
+                //if file is empty, storage.size = 0, therefore this option wont work
+                //error guard to prevent user from using this function
+                if (quotes.storage.size() <= 0)
+                {
+                    JOptionPane.showMessageDialog(null, "Error!\nThere are no quotes to randomly display!");
+                }
+                else
+                {
+                    //rng variable stores random index value to choose a random quote
+                    int rng = (int) (Math.random() * quotes.storage.size());
+                    System.out.println("============================================");
+                    System.out.println(quotes.storage.get(rng));
+                    System.out.println("============================================");
+                }
 
-			}
+            }
 
-			// DISPLAY RANDOM QUOTE
-			else if (userDialog == 1) {
-				int rng = (int) (Math.random() * quotes.storage.size());
-				System.out.println("============================================");
-				System.out.println(quotes.storage.get(rng));
-				System.out.println("============================================");
+            // DISPLAY SELECTED QUOTE
+            else if(userDialog == 2) {
+                if (quotes.storage.size() <= 0)
+                {
+                    JOptionPane.showMessageDialog(null, "Error!\nThere are no quotes to select!");
+                }
+                else
+                {
+                    user = (String) JOptionPane.showInputDialog(null, "Which quote would you like to use?\nClick \"cancel\" to return to the main menu.", "Quotations Database", JOptionPane.PLAIN_MESSAGE, null, quotes.storage.toArray(), quotes.storage.get(0));
+                    if (user != null){
+                    System.out.println("============================================");
+                    System.out.println(user);
+                    System.out.println("============================================");
+                    }
+                }
+            }
+            //SEARCH BY AUTHOR
+            else if (userDialog == 3)
+            {
+                if (quotes.storage.size() <= 0)
+                {
+                    JOptionPane.showMessageDialog(null, "Error!\nThere are no authors to select!");
+                }
+                else
+                    {
+                    //generates list of authors for users to select from
+                    quotes.authorIndex();
+                    //user input
+                    authName = (String) JOptionPane.showInputDialog(null, "Please select the author of the quotes you are looking for.\nClick \"cancel\" to return to the main menu.", "Quotations Database", JOptionPane.PLAIN_MESSAGE, null, quotes.authIndex.toArray(), quotes.authIndex.get(0));
+                    //gets list of quotes by the specified author
+                    if (authName != null)
+                    {
+                    quotes.search(authName);
+                    }
+                }
 
-			}
+            }
+            // ADD A QUOTE
+            else if(userDialog == 4) {
+                exit = false;
+                while (exit == false)
+                {
+                    quoteInput = JOptionPane.showInputDialog("Please input the quote you would like to add.\nClick \"cancel\" to return to the main menu.");
+                    if ( quoteInput == null)
+                    {
+                        exit = true;
+                    }
+                    else if (quoteInput.equalsIgnoreCase(""))
+                    {
+                        JOptionPane.showMessageDialog(null,"Error! this field cannot be BLANK!");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                while (exit == false)
+                {
+                   authName = JOptionPane.showInputDialog("Please input the author of the quote you would like to add.\nClick \"cancel\" to return to the main menu.");
+                   if (authName == null)
+                    {
+                        exit = true;
+                    }
+                    else if (authName.equalsIgnoreCase(""))
+                    {
+                        JOptionPane.showMessageDialog(null,"Error! this field cannot be BLANK!");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (exit == false)
+                {
+                    quotes.addQuote(authName, quoteInput);
+                }
+            }
 
-			// DISPLAY SELECTED QUOTE
-			else if (userDialog == 2) {
-				user = (String) JOptionPane.showInputDialog(null, "Which quote would you like to use?",
-						"Quotations Database", JOptionPane.PLAIN_MESSAGE, null, quotes.storage.toArray(),
-						quotes.storage.get(0));
-				System.out.println("============================================");
-				System.out.println(user);
-				System.out.println("============================================");
-			}
-			// SEARCH BY AUTHOR
-			else if (userDialog == 3) {
-				while (true) {
-					authName = JOptionPane
-							.showInputDialog("Please input the author of the quote you would like to add.");
-					if (authName.equalsIgnoreCase(null) || authName.equalsIgnoreCase("")) {
-						JOptionPane.showMessageDialog(null, "Error! this field cannot be BLANK!");
-					} 
-					else {
-						break;
-					}
-				}
-				quotes.search(authName);
-			}
-			// ADD A QUOTE
-			else if (userDialog == 4) {
-				while (true) {
-					quoteInput = JOptionPane.showInputDialog("Please input the quote you would like to add.");
-					if (quoteInput.equalsIgnoreCase(null) || quoteInput.equalsIgnoreCase("")) {
-						JOptionPane.showMessageDialog(null, "Error! this field cannot be BLANK!");
-					} 
-					else {
-						break;
-					}
-				}
-				while (true) {
-					authName = JOptionPane
-							.showInputDialog("Please input the author of the quote you would like to add.");
-					if (authName.equalsIgnoreCase(null) || authName.equalsIgnoreCase("")) {
-						JOptionPane.showMessageDialog(null, "Error! this field cannot be BLANK!");
-					} 
-					else {
-						break;
-					}
-				}
-				quotes.addQuote(authName, quoteInput);
-			}
-
-			// REMOVE A QUOTE
-			else if (userDialog == 5) {
-				removeInput = (String) JOptionPane.showInputDialog(null, "Which quote would you like to use?",
-						"Quotations Database", JOptionPane.PLAIN_MESSAGE, null, quotes.storage.toArray(),
-						quotes.storage.get(0));
-				quotes.removeQuote(removeInput);
-			}
-			// EXIT MENU AND THEN REWRITE TXT FILE
-
-		}
-		System.exit(0);
-
-	}
-
+            // REMOVE A QUOTE
+            else if(userDialog == 5) {
+                if (quotes.storage.size() <= 0)
+                {
+                    JOptionPane.showMessageDialog(null, "Error!\nThere are no quotes to remove!");
+                }
+                else
+                {
+                    removeInput =(String)JOptionPane.showInputDialog(null, "Which quote would you like to use?\nClick \"cancel\" to return to the main menu.", "Quotations Database", JOptionPane.PLAIN_MESSAGE, null, quotes.storage.toArray(), quotes.storage.get(0));
+                    if (removeInput != null){
+                        quotes.removeQuote(removeInput);
+                    }
+                }
+            }
+        }
+        fileWriter1 = new PrintWriter(new FileWriter(location));
+        //counter for quotes, stops loop when there are no more quotes to add
+        for (int j = 0; j < quotes.storage.size(); j++)
+        {
+            //adds a quote from quote arraylist
+            fileWriter1.println(quotes.storage.get(j));
+            //then adds its author from the author arraylist
+            fileWriter1.println("- "+quotes.authorList.get(j));
+        }
+        //closes filewriter
+        fileWriter1.close();
+        //exits program
+        System.exit(0);
+    }
 }
